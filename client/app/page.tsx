16f2aad9c24acde0529a2667a78f669bf0a4b9ac
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2025-02-19 21:46:32 -0600

    Update tab labels to use 'HyperGO' terminology

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <Tabs defaultValue="create" className="space-y-8">
       <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="create">Create Shortcut</TabsTrigger>
-        <TabsTrigger value="list">View Shortcuts</TabsTrigger>
+        <TabsTrigger value="create">Create A HyperGO</TabsTrigger>
+        <TabsTrigger value="list">View HyperGOs</TabsTrigger>
       </TabsList>
       <TabsContent value="create">
         <ShortcutForm />
