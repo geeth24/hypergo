e0dadb0faa75fb782206887f9bc8c3e247e24d72
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2026-03-09 03:36:14 -0500

    fix: update go host IP and add local ingress rule

--- add-go-host.sh
+++ add-go-host.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Adding go to hosts file..."
-sudo sh -c 'echo "192.168.1.71 go" >> /etc/hosts'
+sudo sh -c 'echo "192.168.1.69 go" >> /etc/hosts'
 echo "Done! Try using http://go in your browser"
